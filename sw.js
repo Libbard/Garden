1a544afa725c0938c89c3937c28644123401eec4
Libbard <9252806+Libbard@users.noreply.github.com>
2026-08-17 05:46:30 +0300

    عامل الخدمة: مزامنة بصمات التخزين المسبق

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('shared/reminders-db.js');
 
 /*@0.SWJ.109*/
-var SW_VERSION = 'garden-1.0.0.62'; /*@0.SWJ.2*/
+var SW_VERSION = 'garden-1.0.0.63'; /*@0.SWJ.2*/
 var CACHE_NAME = 'garden-static';
 var ADOPT_PREFIX = CACHE_NAME.replace(/static$/, '');
 /*@0.SWJ.110*/
