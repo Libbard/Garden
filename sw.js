e1c9fa6d527c68e7d49adfdd01d1a6939009fd04
Libbard <9252806+Libbard@users.noreply.github.com>
2026-08-31 16:50:00 +0300

    عامل الخدمة: ترقية إصدار التخزين المسبق

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('shared/reminders-db.js');
 
 /*@0.SWJ.109*/
-var SW_VERSION = 'garden-1.0.3.97'; /*@0.SWJ.2*/
+var SW_VERSION = 'garden-1.0.3.98'; /*@0.SWJ.2*/
 var CACHE_NAME = 'garden-static';
 var ADOPT_PREFIX = CACHE_NAME.replace(/static$/, '');
 /*@0.SWJ.110*/
